38427e1960bf1725eb87965e745a2a4596a6c43c
travisburns <41456635+travisburns@users.noreply.github.com>
2022-07-16 17:30:36 -0700

    Add files via upload
    Simple Guessing Game using c#

---
+++ guessinggame/guessinggame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+            while (guess != secretWord && !outOfGuesses) 
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                } 
+                else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if(outOfGuesses)
+            {
+                Console.Write("You Lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
